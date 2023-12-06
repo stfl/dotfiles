7c8f6924e59eb0738f0cecb1fa88ce70fde96a39
Stefan Lendl <s.lendl@proxmox.com>
2023-12-06 15:30:20 +0100

    startsway.sh to unlock verious keyrings before launching sway

--- config/startsway.sh
+++ config/startsway.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env zsh
 
-set -euxo pipefail
+# set -uo pipefail
 
 setopt extended_glob
-echo "Unlock ssh keyring"
+echo "---- Unlock ssh keyring:"
 ssh-add ~/.ssh/id_*~*.pub
 
 # echo "Unlock gpg keyring"
 
-echo "Unlock Bitwarden"
+echo -e "\n---- Unlock Bitwarden"
 rbw unlock
 
-echo "Enter LDAP password to access alma shares"
+echo -e "\n---- Store LDAP password to access alma shares"
 cifscreds add -u slendl alma.intra.proxmox.com
 
-echo "---- Starting Sway ----" >>| ~/.cache/sway.log
-nixGLIntel sway &>> ~/.cache/sway.log
+echo -e "\n---- Starting Sway"
+is_sway=$(systemctl --user is-active sway-session.target)
+print "sway-session is $is_sway"
+
+if [[ x$is_sway != xactive ]]; then
+    echo "---- Starting Sway ----" >>| ~/.cache/sway.log
+    nixGLIntel sway &>> ~/.cache/sway.log
+else
+    echo "not starting"
+    exit 0
+fi
