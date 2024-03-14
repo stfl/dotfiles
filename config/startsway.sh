6a1b5356c436e7966c7c91c518294b3c702a9e40
Stefan Lendl <git@stfl.dev>
2024-03-14 15:25:14 +0100

    do not add ssh keys in startsway.sh
    Signed-off-by: Stefan Lendl <git@stfl.dev>

--- config/startsway.sh
+++ config/startsway.sh
@@ -3,22 +3,17 @@
 # set -uo pipefail
 
 setopt extended_glob
-echo "---- Unlock ssh keyring:"
-ssh-add ~/.ssh/id_*~*.pub
-
-# echo "Unlock gpg keyring"
-
 echo -e "\n---- Unlock Bitwarden"
 rbw unlock
 
 echo -e "\n---- Store LDAP password to access alma shares"
 cifscreds add -u slendl alma.intra.proxmox.com
 
-echo -e "\n---- Starting Sway"
 is_sway=$(systemctl --user is-active sway-session.target)
 print "sway-session is $is_sway"
 
 if [[ x$is_sway != xactive ]]; then
+    echo -e "\n---- Starting Sway"
     echo "---- Starting Sway ----" >>| ~/.cache/sway.log
     nixGLIntel sway &>> ~/.cache/sway.log
 else
