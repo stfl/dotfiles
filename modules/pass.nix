683fdb0824d579698e89fea891a1567324e7f5a4
Stefan Lendl <git@stfl.dev>
2024-03-14 14:57:19 +0100

    remove commetns
    Signed-off-by: Stefan Lendl <git@stfl.dev>

--- modules/pass.nix
+++ modules/pass.nix
@@ -5,9 +5,7 @@
   ...
 }: {
   home.packages = with pkgs; [
-    # gnome.seahorse
-    # pass-wayland
-    wofi-pass # TODO add key mapping to sway!!
+    wofi-pass
 
     # pass-secret-service
     # passExtensions.pass-otp
@@ -22,19 +20,6 @@
   };
 
   services.pass-secret-service.enable = true;
-
-  # TODO
-  # accounts.email.accounts."proxmox".passwordCommand =
-  #   "${config.programs.rbw.package}/bin/rbw get webmail.proxmox.com";
-
-  # TODO potentially move sway keybind for wofi-pass here
-  # wayland.windowManager.sway.config.keybindings = let
-  #   cfg = config.wayland.windowManager.sway;
-  #   modifier = cfg.config.modifier;
-  # in {
-  #   # wofi-pass
-  #   "${modifier}+g" = "exec --no-startup-id wofi-pass --autotype";
-  # };
 
   programs.browserpass = {
     enable = true;
