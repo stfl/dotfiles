aba16af60ef588bf5c476809503320f7997d243b
Stefan Lendl <git@stfl.dev>
2024-03-14 15:55:25 +0100

    comment out cava, not using this
    Signed-off-by: Stefan Lendl <git@stfl.dev>

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -480,16 +480,16 @@ in {
     style = ../config/waybar.css;
   };
 
-  programs.cava = {
-    enable = true;
-    settings = {
-      general.framerate = 60;
-      input.method = "alsa";
-      smoothing.noise_reduction = 88;
-      color = {
-        background = "'#000000'";
-        foreground = "'#FFFFFF'";
-      };
-    };
-  };
+  # programs.cava = {
+  #   enable = true;
+  #   settings = {
+  #     general.framerate = 60;
+  #     input.method = "alsa";
+  #     smoothing.noise_reduction = 88;
+  #     color = {
+  #       background = "'#000000'";
+  #       foreground = "'#FFFFFF'";
+  #     };
+  #   };
+  # };
 }
