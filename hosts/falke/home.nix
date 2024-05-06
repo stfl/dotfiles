266a8af456f964546fc1e2a05fae4a83d02c3b49
Stefan Lendl <git@stfl.dev>
2024-05-06 09:50:21 +0200

    falke: initial setup

---
+++ hosts/falke/home.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+in {
+  imports = [
+    ../../modules/home/common.nix
+    ../../modules/home/desktop.nix
+    ../../modules/home/emacs.nix
+    ../../modules/home/pass.nix
+  ];
+
+  home.stateVersion = "23.11";
+
+  home.packages = with pkgs; [
+    nvtopPackages.intel
+  ];
+
+  programs.waybar.settings.mainBar = {
+    network.on-click = "nm-connection-editor";
+  };
+
+  wayland.windowManager.sway.extraOptions = [
+    "--unsupported-gpu"
+  ];
+}
