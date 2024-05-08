26d8921a9b33e908a2cd407e72ea647df68a5588
Stefan Lendl <git@stfl.dev>
2024-05-08 14:41:09 +0200

    falke: kanshi config

--- hosts/falke/home.nix
+++ hosts/falke/home.nix
@@ -21,4 +21,23 @@ in {
     network.on-click = "nm-connection-editor";
     temperature.hwmon-path = "/sys/class/hwmon/hwmon4/temp1_input";
   };
+
+  services.kanshi = {
+    enable = true;
+    profiles = {
+      standalone.outputs = [
+        {criteria = "eDP-1";}
+      ];
+      docked_3datax = {
+        outputs = [
+          {criteria = "eDP-1";}
+          {criteria = "DP-6";}
+          {
+            criteria = "DP-7";
+            transform = "90";
+          }
+        ];
+      };
+    };
+  };
 }
