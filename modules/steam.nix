f60367fb76d653efcccc3cf0cc38c1ce76b5f61d
Stefan Lendl <git@stfl.dev>
2024-06-24 23:59:32 +0200

    steam: install magemode and mangohud

--- modules/steam.nix
+++ modules/steam.nix
@@ -6,9 +6,20 @@
 }: {
   programs.steam = {
     enable = true;
+    # gameScopeSession.enable = true;
+    extraPackages = with pkgs; [
+      # gamescope
+      # gamemode
+      mangohud
+      # openssl_1_1
+    ];
   };
 
+  programs.gamemode.enable = true;
+  # programs.gamemode.settings.general.inhibit_screensaver = 0;
+
   # packages = with pkgs; [
-  #   steam-run
+  # steam-run
+  # mangohud
   # ];
 }
