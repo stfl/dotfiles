bd8c80bad32d23e7179f9f7b42077aab8925424d
Stefan Lendl <git@stfl.dev>
2024-09-05 17:04:11 +0200

    kondor: add more wayland variables (not tested)

--- hosts/kondor/home.nix
+++ hosts/kondor/home.nix
@@ -23,10 +23,20 @@ in {
     ];
     extraSessionCommands = ''
       # Nvidia specific config
+      # Hardware cursors not yet working on wlroots
       export WLR_NO_HARDWARE_CURSORS=1
+      # Set wlroots renderer to Vulkan to avoid flickering
+      export WLR_RENDERER=vulkan
+
+      # OpenGL Variables
       export GBM_BACKEND=nvidia-drm
-      export GBM_BACKENDS_PATH=/etc/gbm
+      export __GL_GSYNC_ALLOWED=0
+      export __GL_VRR_ALLOWED=0
       export __GLX_VENDOR_LIBRARY_NAME=nvidia
+      # export GBM_BACKENDS_PATH=/etc/gbm
+
+      # Xwayland compatibility
+      export XWAYLAND_NO_GLAMOR=1
     '';
   };
 }
