e4e2fe2f2a1ef0a52df08c552388260788169563
Stefan Lendl <git@stfl.dev>
2024-09-23 21:40:41 +0200

    kondor: enable AMD gpu

--- hosts/kondor/hardware-configuration.nix
+++ hosts/kondor/hardware-configuration.nix
@@ -13,14 +13,6 @@
   ];
 
   boot.initrd.availableKernelModules = ["nvme" "ahci" "xhci_pci" "usbhid" "usb_storage" "sd_mod" "sr_mod"];
-  boot.initrd.kernelModules = [
-    "nvidia"
-    "nvidia_drm"
-    # "nvidia_modeset"
-    # "nvidia_uvm"
-  ];
-  boot.kernelModules = ["kvm-amd"];
-  boot.kernelParams = ["nvidia-drm.fbdev=1"];
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
@@ -46,47 +38,25 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  # NVidia
-  # services.xserver.videoDrivers = ["nouveau"]; # need the xserver setting even for a wayland system
-  services.xserver.videoDrivers = ["nvidia"]; # need the xserver setting even for a wayland system
-  hardware.nvidia = {
-    # Modesetting is required.
-    modesetting.enable = true;
+  # Radeon
+  services.xserver.videoDrivers = lib.mkDefault ["modesetting"];
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
-    powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    nvidiaPersistenced = true;
-
-    # prime.sync.enable = true;
-
-    # slower but may solve the screen tearing issues
-    forceFullCompositionPipeline = true;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
-    # package = config.boot.kernelPackages.nvidiaPackages.production;
-    # package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+  hardware.graphics = {
+    enable = lib.mkDefault true;
+    enable32Bit = lib.mkDefault true;
   };
+
+  hardware.amdgpu.initrd.enable = lib.mkDefault true;
+
+  systemd.tmpfiles.rules = [
+    # "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
+  ];
+
+  # hardware.opengl.extraPackages = with pkgs; [
+  #   rocmPackages.clr.icd
+  # ];
+
+  # environment.systemPackages = with pkgs; [
+  #   clinfo
+  # ];
 }
--- hosts/kondor/home.nix
+++ hosts/kondor/home.nix
@@ -14,29 +14,6 @@ in {
   ];
 
   home.packages = with pkgs; [
-    nvtopPackages.nvidia
+    nvtopPackages.amd
   ];
-
-  wayland.windowManager.sway = {
-    extraOptions = [
-      "--unsupported-gpu"
-    ];
-    extraSessionCommands = ''
-      # Nvidia specific config
-      # Hardware cursors not yet working on wlroots
-      export WLR_NO_HARDWARE_CURSORS=1
-      # Set wlroots renderer to Vulkan to avoid flickering
-      export WLR_RENDERER=vulkan
-
-      # OpenGL Variables
-      export GBM_BACKEND=nvidia-drm
-      export __GL_GSYNC_ALLOWED=0
-      export __GL_VRR_ALLOWED=0
-      export __GLX_VENDOR_LIBRARY_NAME=nvidia
-      # export GBM_BACKENDS_PATH=/etc/gbm
-
-      # Xwayland compatibility
-      export XWAYLAND_NO_GLAMOR=1
-    '';
-  };
 }
