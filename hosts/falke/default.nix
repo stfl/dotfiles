f39edf66d412f0d61ef8eded5898b5a3865c50aa
Stefan Lendl <git@stfl.dev>
2024-09-25 16:28:55 +0200

    falke: remove udev-based auto-hibernate

--- hosts/falke/default.nix
+++ hosts/falke/default.nix
@@ -268,8 +268,4 @@ in {
 
   # Suspend the system when battery level drops to 5% or lower
   services.autoSuspend.enable = true;
-
-  services.udev.extraRules = ''
-    SUBSYSTEM=="power_supply", ATTR{status}=="Discharging", ATTR{capacity}=="[0-5]", RUN+="${pkgs.systemd}/bin/systemctl hibernate"
-  '';
 }
