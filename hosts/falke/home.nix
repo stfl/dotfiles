b793f0439b3043e323501d3fb2370f40977cce1b
Stefan Lendl <git@stfl.dev>
2024-10-02 14:35:38 +0200

    kanshi: add home office setup for falke

--- hosts/falke/home.nix
+++ hosts/falke/home.nix
@@ -25,22 +25,28 @@ with lib; {
   services.kanshi = {
     enable = true;
     settings = let
-      rotated_output = "Dell Inc. DELL U2518D 3C4YP8AV547L";
-      center_output = "Dell Inc. DELL U2518D 3C4YP8AV590L";
+      office_rotated_output = "Dell Inc. DELL U2518D 3C4YP8AV547L";
+      office_center_output = "Dell Inc. DELL U2518D 3C4YP8AV590L";
+      home_output = "HannStar Display Corp HC284UFB Unknown";
     in [
       {
         output.criteria = "eDP-1";
         output.alias = "builtin";
       }
       {
-        output.criteria = "${rotated_output}";
+        output.criteria = "${office_rotated_output}";
         output.transform = "90";
         output.alias = "rotated";
       }
       {
-        output.criteria = "${center_output}";
+        output.criteria = "${office_center_output}";
         output.transform = "normal";
         output.alias = "center";
+      }
+      {
+        output.criteria = "${home_output}";
+        output.transform = "normal";
+        output.alias = "home";
       }
       {
         profile.name = "standalone";
@@ -49,6 +55,9 @@ with lib; {
             criteria = "$builtin";
             position = "0,0";
           }
+        ];
+        profile.exec = [
+          "${pkgs.libnotify}/bin/notify-send --expire-time=5000 \"Kanshi\" \"Profile standalone applied\""
         ];
       }
       {
@@ -68,16 +77,17 @@ with lib; {
           }
         ];
         profile.exec = [
-          "${pkgs.sway}/bin/swaymsg workspace 1,  move workspace to eDP-1"
-          "${pkgs.sway}/bin/swaymsg workspace 2,  move workspace to eDP-1"
-          "${pkgs.sway}/bin/swaymsg workspace 3,  move workspace to output '\"${rotated_output}\"'"
-          "${pkgs.sway}/bin/swaymsg workspace 4,  move workspace to output '\"${rotated_output}\"'"
-          "${pkgs.sway}/bin/swaymsg workspace 5,  move workspace to output '\"${center_output}\"'"
-          "${pkgs.sway}/bin/swaymsg workspace 6,  move workspace to output '\"${center_output}\"'"
-          "${pkgs.sway}/bin/swaymsg workspace 7,  move workspace to output '\"${center_output}\"'"
-          "${pkgs.sway}/bin/swaymsg workspace 8,  move workspace to output '\"${center_output}\"'"
-          "${pkgs.sway}/bin/swaymsg workspace 9,  move workspace to output '\"${center_output}\"'"
-          "${pkgs.sway}/bin/swaymsg workspace 10, move workspace to output '\"${center_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 1,  move workspace to output '\"${office_center_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 2,  move workspace to output '\"${office_center_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 3,  move workspace to output '\"${office_center_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 4,  move workspace to output '\"${office_rotated_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 5,  move workspace to output '\"${office_rotated_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 6,  move workspace to output '\"${office_rotated_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 7,  move workspace to output '\"${office_rotated_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 8,  move workspace to output '\"${office_rotated_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 9,  move workspace to eDP-1"
+          "${pkgs.sway}/bin/swaymsg workspace 10, move workspace to eDP-1"
+          "${pkgs.libnotify}/bin/notify-send --expire-time=5000 \"Kanshi\" \"Profile 3datax applied\""
         ];
       }
       {
@@ -93,16 +103,43 @@ with lib; {
           }
         ];
         profile.exec = [
-          "${pkgs.sway}/bin/swaymsg workspace 1,  move workspace to eDP-1"
-          "${pkgs.sway}/bin/swaymsg workspace 2,  move workspace to eDP-1"
-          "${pkgs.sway}/bin/swaymsg workspace 3,  move workspace to output '\"${rotated_output}\"'"
-          "${pkgs.sway}/bin/swaymsg workspace 4,  move workspace to output '\"${rotated_output}\"'"
-          "${pkgs.sway}/bin/swaymsg workspace 5,  move workspace to output '\"${rotated_output}\"'"
-          "${pkgs.sway}/bin/swaymsg workspace 6,  move workspace to output '\"${rotated_output}\"'"
-          "${pkgs.sway}/bin/swaymsg workspace 7,  move workspace to output '\"${rotated_output}\"'"
-          "${pkgs.sway}/bin/swaymsg workspace 8,  move workspace to output '\"${rotated_output}\"'"
-          "${pkgs.sway}/bin/swaymsg workspace 9,  move workspace to output '\"${rotated_output}\"'"
-          "${pkgs.sway}/bin/swaymsg workspace 10, move workspace to output '\"${rotated_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 1,  move workspace to output '\"${office_rotated_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 2,  move workspace to output '\"${office_rotated_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 3,  move workspace to output '\"${office_rotated_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 4,  move workspace to output '\"${office_rotated_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 5,  move workspace to output '\"${office_rotated_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 6,  move workspace to output '\"${office_rotated_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 7,  move workspace to output '\"${office_rotated_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 8,  move workspace to output '\"${office_rotated_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 9,  move workspace to eDP-1"
+          "${pkgs.sway}/bin/swaymsg workspace 10, move workspace to eDP-1"
+          "${pkgs.libnotify}/bin/notify-send --expire-time=5000 \"Kanshi\" \"Profile 3datax_half applied\""
+        ];
+      }
+      {
+        profile.name = "home_dock";
+        profile.outputs = [
+          {
+            criteria = "$builtin";
+            position = "0,0";
+          }
+          {
+            criteria = "$home";
+            position = "1920,0";
+          }
+        ];
+        profile.exec = [
+          "${pkgs.sway}/bin/swaymsg workspace 1,  move workspace to output '\"${home_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 2,  move workspace to output '\"${home_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 3,  move workspace to output '\"${home_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 4,  move workspace to output '\"${home_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 5,  move workspace to output '\"${home_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 6,  move workspace to output '\"${home_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 7,  move workspace to output '\"${home_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 8,  move workspace to output '\"${home_output}\"'"
+          "${pkgs.sway}/bin/swaymsg workspace 9,  move workspace to eDP-1"
+          "${pkgs.sway}/bin/swaymsg workspace 10, move workspace to eDP-1"
+          "${pkgs.libnotify}/bin/notify-send --expire-time=5000 \"Kanshi\" \"Profile home_dock applied\""
         ];
       }
     ];
