b99435a89dc5a239ab973c9e256418e0ed566968
Stefan Lendl <git@stfl.dev>
2024-11-18 21:33:15 +0100

    agenix: rekey with kondor ssh host key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,9 @@
 let
   key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINGSjWr1X80phoVLQDpXyn26SAPytikVdGyTK1ifYxR6";
-  falke = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO2sxIJEgEREgxFMaM7rMPNVg8bW1xNydcu9nU6G/AG6";
-  kondor = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDeRsZ7HWpV+CMDTR5TXR7cHPs6uK8tSQ+B3/0GSXjoy";
+
+  # /etc/ssh/ssh_host_ed25519_key.pub
+  falke = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO2sxIJEgEREgxFMaM7rMPNVg8bW1xNydcu9nU6G/AG6"; # TODO falke key does not exist anymore
+  kondor = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIlV7lYyECZGTG1mdp9uj6fkhqS060reE/+v9jZ63dXv";
 in {
   "wg-pulswerk-private.age".publicKeys = [key falke kondor];
   "wg-pulswerk-preshared.age".publicKeys = [key falke kondor];
