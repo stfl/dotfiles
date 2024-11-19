fb7b3a9cc2b331ef0dfdd9a62556e12908dff60d
Stefan Lendl <git@stfl.dev>
2024-11-19 09:10:48 +0100

    desktop: comment out wireplumber bt config because causes crashes

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -33,15 +33,28 @@
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     # media-session.enable = true;
-    wireplumber.configPackages = [
-      (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
-        bluez_monitor.properties = {
-          ["bluez5.enable-sbc-xq"] = true,
-          ["bluez5.enable-msbc"] = true,
-          ["bluez5.enable-hw-volume"] = true,
-          ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-        }
-      '')
-    ];
+    # wireplumber.configPackages = [
+    #   (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
+    #     bluez_monitor.properties = {
+    #       ["bluez5.enable-sbc-xq"] = true,
+    #       ["bluez5.enable-msbc"] = true,
+    #       ["bluez5.enable-hw-volume"] = true,
+    #       ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+    #     }
+    #   '')
+    # ];
   };
+  # services.pipewire.wireplumber.extraScripts.main."99-alsa-lowlatency" = ''
+  #   alsa_monitor.rules = {
+  #     {
+  #       matches = {{{ "node.name", "matches", "alsa_output.*" }}};
+  #       apply_properties = {
+  #         ["audio.format"] = "S32LE",
+  #         ["audio.rate"] = "96000", -- for USB soundcards it should be twice your desired rate
+  #         ["api.alsa.period-size"] = 2, -- defaults to 1024, tweak by trial-and-error
+  #         -- ["api.alsa.disable-batch"] = true, -- generally, USB soundcards use the batch mode
+  #       },
+  #     },
+  #   }
+  # '';
 }
