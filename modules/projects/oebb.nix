15ee60abe4d62cbcbe22e142d1b80ca58dbba987
Stefan Lendl <git@stfl.dev>
2024-11-24 18:50:52 +0100

    oebb: setup split github account
    automatically distinguish github account and rewrite the url

--- modules/projects/oebb.nix
+++ modules/projects/oebb.nix
@@ -4,7 +4,9 @@
   pkgs,
   USER,
   ...
-}: {
+}: let
+  github-host = "digirail.github.com";
+in {
   home-manager.users.${USER} = {
     pkgs,
     config,
@@ -14,11 +16,29 @@
       act
     ];
 
+    programs.git.aliases = {
+      # digirail alias to apply config for individual commands:
+      # `git digirail -- clone git@github.com`
+      #  -> automatically rewrites to a git clone git@digirail.github.com
+      digirail = "!sh -c 'git -c url.git@${github-host}:.insteadOf=git@github.com: -c url.git@${github-host}:.pushInsteadOf=git@github.com: \"$@\"'";
+    };
+
     programs.git.includes = [
       {
         condition = "gitdir:${config.home.homeDirectory}/work/oebb/";
         contents.user.email = "stefan-digirailbox@stfl.dev";
+        contents.url."git@${github-host}:" = {
+          insteadOf = "git@github.com:";
+          pushInsteadOf = "git@github.com:";
+        };
       }
     ];
+
+    programs.ssh.matchBlocks."${github-host}" = {
+      hostname = "github.com";
+      user = "git";
+      identityFile = ["${config.home.homeDirectory}/.ssh/id_ed25519_oebb"];
+      identitiesOnly = true;
+    };
   };
 }
