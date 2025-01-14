dfde06b12416cdd0d21c57697eae3d4117daa20b
Stefan Lendl <git@stfl.dev>
2025-01-14 11:26:35 +0100

    docker: assert !nftables

--- modules/docker.nix
+++ modules/docker.nix
@@ -1,4 +1,5 @@
 {
+  config,
   pkgs,
   USER,
   ...
@@ -8,4 +9,16 @@
   environment.systemPackages = with pkgs; [docker-compose];
 
   users.users.${USER}.extraGroups = ["docker"];
+
+  assertions = [
+    {
+      assertion = !config.networking.nftables.enable;
+      message = ''
+        Docker does not support nftables.
+        You have enabled both:
+          - virtualisation.docker.enable = true
+          - networking.nftables.enable = true
+      '';
+    }
+  ];
 }
