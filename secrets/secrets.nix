e917c25343b04cca5c48882e9f14f0f20db90feb
Stefan Lendl <git@stfl.dev>
2025-02-17 23:03:19 +0100

    agenix: pirol secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,12 +3,13 @@ let
 
   # /etc/ssh/ssh_host_ed25519_key.pub
   falke = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO2sxIJEgEREgxFMaM7rMPNVg8bW1xNydcu9nU6G/AG6"; # TODO falke key does not exist anymore
+  pirol = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM+lyPjaizlwlOz9KndAlv+HtUjl5rzXwzbXasB4soe2";
   kondor = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIlV7lYyECZGTG1mdp9uj6fkhqS060reE/+v9jZ63dXv";
 in {
-  "wg-pulswerk-private.age".publicKeys = [key falke kondor];
-  "wg-pulswerk-preshared.age".publicKeys = [key falke kondor];
-  "wg-hei-private.age".publicKeys = [key falke kondor];
-  "wg-hei-preshared.age".publicKeys = [key falke kondor];
+  "wg-pulswerk-private.age".publicKeys = [key pirol kondor];
+  "wg-pulswerk-preshared.age".publicKeys = [key pirol kondor];
+  "wg-hei-private.age".publicKeys = [key pirol kondor];
+  "wg-hei-preshared.age".publicKeys = [key pirol kondor];
   "3datax-ssh-config.age".publicKeys = [key falke];
-  "wg-digirail-private.age".publicKeys = [key kondor];
+  "wg-digirail-private.age".publicKeys = [key pirol kondor];
 }
