452879f326d7064bf78bc2e7dac8d350a2b69b45
Stefan Lendl <git@stfl.dev>
2025-05-05 09:39:39 +0200

    steam settings

--- modules/steam.nix
+++ modules/steam.nix
@@ -7,19 +7,41 @@
     gamescope = {
       enable = true;
       capSysNice = true;
+      args = [
+        "--adaptive-sync"
+        # "--hdr-enabled"
+        "--rt"
+        "--mangoapp"
+      ];
     };
     gamemode = {
       enable = true;
       enableRenice = true;
-      settings.general.renice = 10;
+      settings.general.renice = 20;
     };
     steam = {
       enable = true;
-      gamescopeSession.enable = true;
+      gamescopeSession = {
+        # to launch with `steam-gamescope``
+        enable = true;
+        args = [
+          "--adaptive-sync"
+          "--rt"
+          "--mangoapp"
+
+          "--borderless"
+          "--scale-to-output 1.0"
+          "--scale-to-window 1.0"
+          "--session-name steam"
+
+          # todo make this output dependent or something
+          # "-W 3840"
+          # "-H 2160"
+        ];
+      };
       extraPackages = with pkgs; [
         gamescope-wsi
-        # gamescope
-        # gamemode
+        gamemode
         mangohud
         steamtinkerlaunch
 
