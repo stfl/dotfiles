3a854ec7e2bf335568d60ff3117cbb7d659b5e94
Stefan Lendl <git@stfl.dev>
2025-05-28 09:10:46 +0200

    renamed glibc locales

--- hosts/kondor/default.nix
+++ hosts/kondor/default.nix
@@ -51,18 +51,18 @@
   time.timeZone = "Europe/Vienna";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.utf8";
+  i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_AT.utf8";
-    LC_IDENTIFICATION = "de_AT.utf8";
-    LC_MEASUREMENT = "de_AT.utf8";
-    LC_MONETARY = "de_AT.utf8";
-    LC_NAME = "de_AT.utf8";
-    LC_NUMERIC = "de_AT.utf8";
-    LC_PAPER = "de_AT.utf8";
-    LC_TELEPHONE = "de_AT.utf8";
-    LC_TIME = "de_AT.utf8";
+    LC_ADDRESS = "de_AT.UTF-8";
+    LC_IDENTIFICATION = "de_AT.UTF-8";
+    LC_MEASUREMENT = "de_AT.UTF-8";
+    LC_MONETARY = "de_AT.UTF-8";
+    LC_NAME = "de_AT.UTF-8";
+    LC_NUMERIC = "de_AT.UTF-8";
+    LC_PAPER = "de_AT.UTF-8";
+    LC_TELEPHONE = "de_AT.UTF-8";
+    LC_TIME = "de_AT.UTF-8";
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
