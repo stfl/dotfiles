02e4fefa255dc9ccd0d198e366bd6f56582b83ab
Stefan Lendl <git@stfl.dev>
2026-01-06 19:27:27 +0100

    remove direnv anaconda layout

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -68,23 +68,6 @@ with lib;
     enable = true;
     enableZshIntegration = true;
     nix-direnv.enable = true;
-    stdlib = ''
-      layout_anaconda() {
-        local ACTIVATE="${config.home.homeDirectory}/.anaconda3/bin/activate"
-
-        if [ -n "$1" ]; then
-          # Explicit environment name from layout command.
-          local env_name="$1"
-          source $ACTIVATE $env_name
-        elif (grep -q name: environment.yml); then
-          # Detect environment name from `environment.yml` file in `.envrc` directory
-          source $ACTIVATE `grep name: environment.yml | sed -e 's/name: //' | cut -d "'" -f 2 | cut -d '"' -f 2`
-        else
-          (>&2 echo No environment specified);
-          exit 1;
-        fi;
-      }
-    '';
   };
 
   programs.git = {
