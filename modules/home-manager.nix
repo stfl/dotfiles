4ee6f253250893fd9fe0b964db3e99979ea85cd9
Stefan Lendl <git@stfl.dev>
2026-02-02 02:09:50 +0100

    enable home-manager agenix module

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -1,9 +1,13 @@
 # Home-manager specific configuration
 # Only import this module in hosts that use home-manager
+{ agenix, ... }:
 {
   home-manager = {
     useUserPackages = true;
     useGlobalPkgs = true;
     backupFileExtension = "backup";
+    sharedModules = [
+      agenix.homeManagerModules.default
+    ];
   };
 }
