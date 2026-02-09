ea5a3e42ec17b8bb7c24380586e8a95c53bd6a55
Stefan Lendl <git@stfl.dev>
2026-02-09 15:09:32 +0100

    install latest zfs compatible kernel

--- modules/hardware/zfs.nix
+++ modules/hardware/zfs.nix
@@ -4,7 +4,24 @@
   pkgs,
   ...
 }:
+let
+  zfsCompatibleKernelPackages = lib.filterAttrs (
+    name: kernelPackages:
+    (builtins.match "linux_[0-9]+_[0-9]+" name) != null
+    && (builtins.tryEval kernelPackages).success
+    && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
+  ) pkgs.linuxKernel.packages;
+  latestKernelPackage = lib.last (
+    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
+      builtins.attrValues zfsCompatibleKernelPackages
+    )
+  );
+in
 {
+  # Use the latest kernel version that ZFS supports
+  # This automatically pins to a ZFS-compatible kernel
+  boot.kernelPackages = latestKernelPackage;
+
   boot.supportedFilesystems = [ "zfs" ];
   # boot.zfs.forceImportRoot = lib.mkDefault false;
 
