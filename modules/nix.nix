e827e93ea5aa2dfb96891674082e4d2f52157185
Stefan Lendl <git@stfl.dev>
2026-02-09 15:09:43 +0100

    comment out cache.flakehub.com because I don't have a subscription

--- modules/nix.nix
+++ modules/nix.nix
@@ -28,7 +28,7 @@
       trusted-substituters = ["https://hyprland.cachix.org"];
       trusted-public-keys = [
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-        "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
+        # "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
