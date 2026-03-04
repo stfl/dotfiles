be6dcc1e836a6a2dda89ef6671ac951a318a1159
Stefan Lendl <git@stfl.dev>
2026-03-04 15:21:17 +0100

    delete copilot-api package

--- packages/default.nix
+++ packages/default.nix
@@ -1,5 +1,3 @@
 final: prev: {
-  copilot-api = final.callPackage ./copilot-api {};
-  # Future packages go here:
-  # my-tool = final.callPackage ./my-tool {};
+  # copilot-api = final.callPackage ./copilot-api {};
 }
